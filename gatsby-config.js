aec59422584a19bfa3f0d4f574b36cef521617ca
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-10-15 21:07:16 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby + Netlify CMS Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
+    {
+      resolve: `@wapps/gatsby-plugin-material-ui`,
+      options: {
+        theme: {
+          primaryColor: '#113643',
+        }
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: [
+            'Roboto',
+            'Material Icons',
+          ]
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
+  ],
+}
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.45,
+  headerFontFamily: [ 'Roboto' ],
+  bodyFontFamily: [ 'Roboto', 'sans serif' ],
+});
+
+ export default typography;
