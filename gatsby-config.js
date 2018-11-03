0833438db41f3dc6e035de40adc168d7836f27c5
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-11-03 14:30:54 -0400

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,19 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-128084522-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        siteSpeedSampleRate: 10,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
