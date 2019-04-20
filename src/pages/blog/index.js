8f24542a36e43f3a59de3622ffe68592e57098c9
Aryan Jabbari <ajabbari@atomicsoftware.com>
2019-04-19 21:17:28 -0400

    #15: Add description to blog index page and homepage

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import React from 'react';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 import Grid from '@material-ui/core/Grid';
 import withRoot from '../../withRoot';
 import Card from '../../components/Card';
 
 class IndexPage extends React.Component {
   render() {
-    const { data: { allMarkdownRemark: { edges: posts }}} = this.props
+    const {
+      data: {
+        allMarkdownRemark: { edges: posts },
+      },
+    } = this.props;
 
     return (
-      <Grid spacing={16} justify="center" container>
-        {posts.map(({ node: post }) => (
-          <Grid item xs={11} sm={10} key={post.id}>
-            <Card post={post} />
-          </Grid>
-        ))}
-      </Grid>
-    )
+      <>
+        <Helmet>
+          <meta name="Description" content="The WebDev Coach blog index page" />
+        </Helmet>
+        <Grid spacing={16} justify="center" container>
+          {posts.map(({ node: post }) => (
+            <Grid item xs={11} sm={10} key={post.id}>
+              <Card post={post} />
+            </Grid>
+          ))}
+        </Grid>
+      </>
+    );
   }
 }
 
@@ -27,15 +37,15 @@ IndexPage.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export default withRoot(IndexPage);
 
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
@@ -53,4 +63,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
